4990102615c2613db57cf8780cce0d9d8912e05d
hjs011 <97939170+hjs011@users.noreply.github.com>
2022-02-05 21:07:58 +0900

    Add files via upload

---
+++ 0205/BOJ_4949_균형잡힌세상_한진성.java
@@ -0,0 +1,57 @@
+package com.ssafy.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_4949_균형잡힌세상 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		go:while (true) {
+			// (), [] 담을 스택
+			Stack<Integer> stack1 = new Stack<>();
+
+			char[] chars = in.readLine().toCharArray();
+			// 종료 조건
+			if (chars.length == 1 && chars[0] == '.') {
+				break;
+			}
+			for (int i = 0; i < chars.length; i++) {
+				switch (chars[i]) {
+				case '(':
+					stack1.push((int) chars[i]);
+					break;
+				case ')':
+					if (!stack1.empty()&&stack1.peek() == '(') {
+						stack1.pop();
+					} else {
+						System.out.println("no");
+						continue go;
+					}
+					break;
+				case '[':
+					stack1.push((int) chars[i]);
+					break;
+				case ']':
+					if (!stack1.empty()&&stack1.peek() == '[') {
+						stack1.pop();
+					} else {
+						System.out.println("no");
+						continue go;
+					}
+					break;
+				}
+			}
+			if (stack1.empty()) {
+				System.out.println("yes");
+			}else {
+				System.out.println("no");
+			}
+		}
+	}
+
+}
