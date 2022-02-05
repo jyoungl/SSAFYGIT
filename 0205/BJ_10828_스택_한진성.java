5403a468e3a03beae0510ab4d6d0603fea2e22f0
hjs011 <97939170+hjs011@users.noreply.github.com>
2022-02-05 21:09:32 +0900

    Update and rename BOJ_10828_스택_한진성.java to BJ_10828_스택_한진성.java

--- 0205/BJ_10828_스택_한진성.java
+++ 0205/BJ_10828_스택_한진성.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class BOJ_10828_스택 {
+public class BJ_10828_스택 {
 
 	static ArrayList<Integer> stack = new ArrayList<>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
