4d4052703bf7f6c522c374e2ffc7932652c48c50
hxxejx <uiu1442@naver.com>
2022-03-08 21:19:37 +0900

    0308 스터디 숙제

--- 0308/BJ_9184_신나는함수실행_허은지.java
+++ 0308/BJ_9184_신나는함수실행_허은지.java
@@ -14,9 +14,11 @@ import java.util.*;
  * 2. 하나라도 20보다 크면 리턴 w(20, 20, 20)
  * 3. a<b<c이면 리턴 w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
  * 4. 그 외는 w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) + w(a-1, b-1, c-1)
+ * 
+ * >> 이미 지나간 번호는 바로 리턴
  */
 public class s2_9184_신나는함수실행 {
-	static int[][][] checked = new int[21][21][21];
+	static int[][][] num = new int[21][21][21];
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,16 +44,16 @@ public class s2_9184_신나는함수실행 {
 		if(a<=0 || b<=0 || c<=0) return 1;
 		
 		//조건 2
-		if(a>20 || b>20 || c>20) return checked[20][20][20] = w(20, 20, 20);
+		if(a>20 || b>20 || c>20) return num[20][20][20] = w(20, 20, 20);
 		
 		//이미 구한 값은 바로 리턴
-		if(checked[a][b][c] != 0) return checked[a][b][c];
+		if(num[a][b][c] != 0) return num[a][b][c];
 		
 		//조건 3
-		if(a<b && b<c) return checked[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		if(a<b && b<c) return num[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
 		
 		//조건 4
-		return checked[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		return num[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
 		
 		
 	}
