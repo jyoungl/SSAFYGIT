622c5cb14529aa9a7becaeb3afe9599df5b52ea7
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-03-15 22:37:19 +0900

    0316 스터디숙제

---
+++ 0316/BJ_1912_부분합_김희주.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1912_부분합 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int[] A = new int[N];
+		for(int i=0;i<N;i++) A[i] = Integer.parseInt(st.nextToken());
+
+		int[] dp = new int[N];
+		dp[0] = A[0];
+
+		int max = dp[0];
+		for(int i=1;i<N;i++){
+			dp[i] = Math.max(dp[i-1]+A[i], A[i]);
+			max = Math.max(dp[i],max);
+		}
+
+		System.out.println(max);
+	}
+}
