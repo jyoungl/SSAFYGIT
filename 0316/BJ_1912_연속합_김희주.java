bff9147a27264ce34beb51d3fefa635f9e50d015
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-03-16 22:36:54 +0900

    rename

--- 0316/BJ_1912_연속합_김희주.java
+++ 0316/BJ_1912_연속합_김희주.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ_1912_부분합 {
+public class BOJ_1912_연속합 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
