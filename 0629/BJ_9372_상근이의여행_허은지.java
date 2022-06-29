a0b95c421e28d07ed005a991bb9f114201f84f53
hxxejx <uiu1442@naver.com>
2022-06-29 16:41:50 +0900

    0629 스터디 문제

---
+++ 0629/BJ_9372_상근이의여행_허은지.java
@@ -0,0 +1,31 @@
+package 문제풀이;
+
+import java.io.*;
+import java.util.*;
+
+public class S4_9372_상근이의여행 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 0; tc < T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			for (int i = 0; i < M; i++) {
+				br.readLine();
+			}
+
+			sb.append((N-1) + "\n");
+			
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
