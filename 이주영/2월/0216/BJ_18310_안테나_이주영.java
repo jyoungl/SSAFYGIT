3a99fe953c2eb42506bd2ca9544ef07c09347af8
jyoungl <50093993+jyoungl@users.noreply.github.com>
2023-02-16 16:14:05 +0900

    0216 스터디 업로드

---
+++ 이주영/2월/0216/BJ_18310_안테나_이주영.java
@@ -0,0 +1,28 @@
+package feb16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_18310_안테나_이주영 {
+    public static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        if (arr.length % 2 == 0) {
+            System.out.println(arr[N / 2 - 1]);
+        }
+        else
+            System.out.println(arr[N/2]);
+    }
+}
